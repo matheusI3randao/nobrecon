d921ab597540bff36bd5f2e3d9cbdd60ee4b1efd
Matheus Brandão <matheus@gmail.com>
2023-10-03 10:38:15 -0300

    Usando express

--- src/webhook.ts
+++ src/webhook.ts
@@ -1,17 +1,17 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Rota para receber as solicitações do webhook
 app.post('/webhook', (req, res) => {
-  const payload = req.body; // O corpo da solicitação contém os dados do webhook
-  console.log('Recebido um webhook:', payload);
-  // Realize as ações desejadas com os dados recebidos aqui
-  res.status(200).end(); // Responda com um status de sucesso
+  const body = req.body;
+
+  // Faça algo com os dados do webhook aqui
+  console.log('Recebido um webhook:', body);
+
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
